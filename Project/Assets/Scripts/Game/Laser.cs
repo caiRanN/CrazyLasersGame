a37d666a95f345c0296ff195109f1dd6a1294a5a
Cairan Steverink <cairan_432@hotmail.com>
2017-02-03 15:26:32 +0100

    Update Laser.cs

--- Project/Assets/Scripts/Game/Laser.cs
+++ Project/Assets/Scripts/Game/Laser.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Laser : MonoBehaviour {
-
 	private void OnTriggerEnter(Collider col) {
 		if(col.tag == "Player") {
 			EventManager<object, string>.LaunchEvent(EventList.GameEnd, this, "You died by a laser!");
